76cef3e343cd628c1012065fa03c7bc5f4fedd2d
Scorpy253 <130481468+Scorpy253@users.noreply.github.com>
2024-12-17 14:35:24 +0200

    Add files via upload

---
+++ Finalized Network Project/networkproject/ChatForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ChatApp
+{
+    partial class ChatForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServerIP = new System.Windows.Forms.TextBox();
+            this.btnStartServer = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtChat = new System.Windows.Forms.ListBox();
+            this.txtMessage = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnConnectClient = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtServerIP
+            // 
+            this.txtServerIP.Location = new System.Drawing.Point(26, 525);
+            this.txtServerIP.Name = "txtServerIP";
+            this.txtServerIP.Size = new System.Drawing.Size(133, 20);
+            this.txtServerIP.TabIndex = 1;
+            // 
+            // btnStartServer
+            // 
+            this.btnStartServer.BackColor = System.Drawing.Color.White;
+            this.btnStartServer.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStartServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnStartServer.Location = new System.Drawing.Point(55, 25);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(75, 32);
+            this.btnStartServer.TabIndex = 3;
+            this.btnStartServer.Text = "Login";
+            this.btnStartServer.UseVisualStyleBackColor = false;
+            this.btnStartServer.Click += new System.EventHandler(this.btnStartServer_Click_1);
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.DarkGray;
+            this.btnSend.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btnSend.Location = new System.Drawing.Point(684, 532);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 51);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send Message";
+            this.btnSend.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click_1);
+            // 
+            // txtChat
+            // 
+            this.txtChat.BackColor = System.Drawing.Color.Teal;
+            this.txtChat.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtChat.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.txtChat.FormattingEnabled = true;
+            this.txtChat.ItemHeight = 20;
+            this.txtChat.Location = new System.Drawing.Point(185, 0);
+            this.txtChat.Name = "txtChat";
+            this.txtChat.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtChat.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.txtChat.Size = new System.Drawing.Size(586, 520);
+            this.txtChat.TabIndex = 6;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(202, 532);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(476, 51);
+            this.txtMessage.TabIndex = 7;
+            this.txtMessage.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(185, 521);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(586, 94);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // btnConnectClient
+            // 
+            this.btnConnectClient.BackColor = System.Drawing.Color.Gray;
+            this.btnConnectClient.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnConnectClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnConnectClient.Location = new System.Drawing.Point(43, 551);
+            this.btnConnectClient.Name = "btnConnectClient";
+            this.btnConnectClient.Size = new System.Drawing.Size(102, 32);
+            this.btnConnectClient.TabIndex = 4;
+            this.btnConnectClient.Text = "Join Chat";
+            this.btnConnectClient.UseVisualStyleBackColor = false;
+            this.btnConnectClient.Click += new System.EventHandler(this.btnConnectClient_Click_1);
+            // 
+            // ChatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(771, 598);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtChat);
+            this.Controls.Add(this.btnConnectClient);
+            this.Controls.Add(this.btnStartServer);
+            this.Controls.Add(this.txtServerIP);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "ChatForm";
+            this.Text = "Messaging App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtServerIP;
+        private System.Windows.Forms.Button btnStartServer;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox txtChat;
+        private System.Windows.Forms.RichTextBox txtMessage;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnConnectClient;
+    }
+}
+
---
+++ Finalized Network Project/networkproject/ChatForm.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatApp
+{
+    public partial class ChatForm : Form
+    {
+        private TcpClient client;
+        private TcpListener listener;
+        private NetworkStream stream;
+
+        private RSACryptoServiceProvider rsa;
+        private string rsaPublicKey;
+        private string rsaPrivateKey;
+        private string receivedRsaPublicKey;
+
+        private Aes aes;
+        private byte[] aesKey;
+
+        private bool CTD;
+
+        public ChatForm()
+        {
+            InitializeComponent();
+
+            // Generate RSA keys
+            rsa = new RSACryptoServiceProvider();
+            rsaPublicKey = rsa.ToXmlString(false); // Public key
+            rsaPrivateKey = rsa.ToXmlString(true); // Private key
+            CTD = false;
+            // Prepare AES for encryption
+            aes = Aes.Create();
+
+            this.FormClosed += ChatForm_FormClosed;
+        }
+
+        private void ChatForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            
+        }
+
+        private async void btnStartServer_Click_1(object sender, EventArgs e)
+        {
+            listener = new TcpListener(IPAddress.Any, 9000);
+            listener.Start();
+            UpdateChat("Server started. Waiting for connection...");
+            txtMessage.Enabled = true;
+            btnSend.Enabled = true;
+            btnConnectClient.Enabled = true;
+            btnSend.BackColor = Color.White;
+            btnConnectClient.BackColor = Color.White;
+            btnStartServer.Enabled = false;
+            btnStartServer.BackColor = Color.Gray;
+            TcpClient client = await listener.AcceptTcpClientAsync();
+            stream = client.GetStream();
+            UpdateChat("Client connected.");
+
+            // Send RSA public key to client
+            SendMessage(rsaPublicKey);
+
+            // Receive client's RSA public key
+            receivedRsaPublicKey = await ReceiveMessageAsync();
+            UpdateChat("Received client's RSA public key.");
+
+            // Encrypt and send AES key using client's RSA public key
+            RSACryptoServiceProvider clientRsa = new RSACryptoServiceProvider();
+            clientRsa.FromXmlString(receivedRsaPublicKey);
+
+            byte[] encryptedAesKey = clientRsa.Encrypt(aes.Key, false);
+            stream.Write(encryptedAesKey, 0, encryptedAesKey.Length);
+            UpdateChat("Sent encrypted AES key to client.");
+
+            await ReceiveMessagesAsync();           
+        }
+
+        private async void btnConnectClient_Click_1(object sender, EventArgs e)
+        {
+            CTD = true;
+            try
+            {
+                client = new TcpClient(txtServerIP.Text, 9000);
+                stream = client.GetStream();
+                btnConnectClient.Enabled = false;
+                btnConnectClient.BackColor = Color.Gray;
+                UpdateChat("Connected to server.");
+
+                // Send RSA public key to server
+                SendMessage(rsaPublicKey);
+
+                // Receive server's RSA public key
+                receivedRsaPublicKey = await ReceiveMessageAsync();
+                UpdateChat("Received server's RSA public key.");
+
+                // Receive encrypted AES key
+                int rsaKeySizeInBytes = rsa.KeySize / 8;
+                byte[] encryptedAesKey = new byte[rsaKeySizeInBytes];
+                int bytesRead = 0;
+                while (bytesRead < encryptedAesKey.Length)
+                {
+                    bytesRead += await stream.ReadAsync(encryptedAesKey, bytesRead, encryptedAesKey.Length - bytesRead);
+                }
+                aesKey = rsa.Decrypt(encryptedAesKey, false);
+                aes.Key = aesKey;
+                UpdateChat("Decrypted AES key.");
+
+                await ReceiveMessagesAsync();
+            }
+            catch (Exception ex)
+            {
+                UpdateChat($"Error: {ex.Message}");
+                CTD = false;
+                btnConnectClient.Enabled = true;
+                btnConnectClient.BackColor = Color.White;
+            }
+        }
+
+        private async Task ReceiveMessagesAsync()
+        {
+            try
+            {
+                while (true)
+                {
+                    var (iv, encryptedMessage) = await ReceiveIvAndEncryptedMessageAsync();
+
+                    aes.IV = iv;
+                    string decryptedMessage = DecryptMessage(encryptedMessage);
+
+                    if (!string.IsNullOrEmpty(decryptedMessage))
+                    {
+                        UpdateChat($"[Client]: {decryptedMessage}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                UpdateChat($"Error receiving message: {ex.Message}");
+            }
+        }
+
+        private async Task<(byte[], byte[])> ReceiveIvAndEncryptedMessageAsync()
+        {
+            try
+            {
+                // Read the IV (16 bytes for AES)
+                byte[] iv = new byte[16];
+                int ivBytesRead = 0;
+                while (ivBytesRead < iv.Length)
+                {
+                    ivBytesRead += await stream.ReadAsync(iv, ivBytesRead, iv.Length - ivBytesRead);
+                }
+
+                // Read the encrypted message
+                List<byte> bufferList = new List<byte>();
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                {
+                    bufferList.AddRange(buffer.Take(bytesRead));
+                    if (bytesRead < buffer.Length)
+                    {
+                        break;
+                    }
+                }
+
+                return (iv, bufferList.ToArray());
+            }
+            catch (Exception ex)
+            {
+                UpdateChat($"Error receiving IV and encrypted message: {ex.Message}");
+                return (null, null);
+            }
+        }
+
+        private string DecryptMessage(byte[] encryptedMessage)
+        {
+            try
+            {
+                ICryptoTransform decryptor = aes.CreateDecryptor();
+                byte[] decryptedBytes = decryptor.TransformFinalBlock(encryptedMessage, 0, encryptedMessage.Length);
+                return Encoding.UTF8.GetString(decryptedBytes);
+            }
+            catch (CryptographicException ex)
+            {
+                UpdateChat($"Decryption error: {ex.Message}");
+                return null;
+            }
+        }
+
+        private async void btnSend_Click_1(object sender, EventArgs e)
+        {
+            if (CTD == true)
+            {
+                string plainText = txtMessage.Text;
+
+                aes.GenerateIV();
+                byte[] iv = aes.IV;
+
+                byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+                ICryptoTransform encryptor = aes.CreateEncryptor();
+                byte[] cipherBytes = encryptor.TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+
+                await stream.WriteAsync(iv, 0, iv.Length);
+                await stream.WriteAsync(cipherBytes, 0, cipherBytes.Length);
+
+                UpdateChat($"[You]: {plainText}");
+                txtMessage.Clear();
+            }           
+        }
+        private async Task<string> ReceiveMessageAsync()
+        {
+            List<byte> bufferList = new List<byte>();
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+            {
+                bufferList.AddRange(buffer.Take(bytesRead));
+                if (bytesRead < buffer.Length)
+                {
+                    break;
+                }
+            }
+            return Encoding.UTF8.GetString(bufferList.ToArray());
+        }
+
+        private void SendMessage(string message)
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            stream.Write(messageBytes, 0, messageBytes.Length);
+        }
+
+        private void UpdateChat(string message)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(UpdateChat), message);
+                return;
+            }
+            txtChat.Items.Add(message + Environment.NewLine);
+            if (txtChat.Items.Count > 24)
+            {
+                txtChat.Items.RemoveAt(0);
+            }
+        }
+    }
+}
